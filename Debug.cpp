1a4da6ee3dcd04e844eb992ee8a017210719dc27
Carla Mishel Segundo Lovera <loveramishel@gmail.com>
2018-11-14 09:16:29 -0600

    first commit

---
+++ Debug.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	using namespace std;
+	for(int i=1;i<=5;i++){
+	
+	cout<<i<<endl;
+	
+	}
+	return 0;
+}
